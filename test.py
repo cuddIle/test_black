5ae097ae4522ecc2351c887203c050011b04ca68
root <root@DESKTOP-M5NA0KP.localdomain>
2024-05-03 12:31:23 +0300

    init

---
+++ test.py
@@ -0,0 +1,28 @@
+def main():
+    pass
+
+
+def a(*args):
+    print(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    )
+
+
+def b():
+    my_variable_is_good_1 = 1
+    my_variable_is_good_2 = 2
+    my_variable_is_good_3 = 3
+    my_variable_is_good_4 = 4
+    my_variable_is_good_5 = 4
+    my_variable_is_good_6 = 4
+    my_variable_is_good_7 = 4
+
+    a(
+        my_variable_is_good_1,
+        my_variable_is_good_2,
+        my_variable_is_good_3,
+        my_variable_is_good_4,
+        my_variable_is_good_5,
+        my_variable_is_good_6,
+        my_variable_is_good_7,
+    )
